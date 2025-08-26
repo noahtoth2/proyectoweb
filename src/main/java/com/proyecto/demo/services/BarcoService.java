66f8b1cc98b789ee67ad9edd4678e65c26a1e963
jtrejos05 <162394617+jtrejos05@users.noreply.github.com>
2025-08-26 22:35:07 +0000

    asdad

--- src/main/java/com/proyecto/demo/services/BarcoService.java
+++ src/main/java/com/proyecto/demo/services/BarcoService.java
@@ -6,45 +6,6 @@ public class BarcoService {
     @Autowired
     private BarcoRepository barcoRepository;
 
-    // Crear
-    public Barco crearBarco(BarcoDTO barco){
-        Barco barco = new Barco(velocidad, posicion, jugador, modelo);
-        barco.setId(nextId++);
-        barcos.add(barco);
-        return barco;
-    }
-
-    // Leer (obtener todos)
-    public List<Barco> obtenerBarcos() {
-        return barcos;
-    }
-
-    // Leer (obtener por id)
-    public Barco obtenerBarcoPorId(int id) {
-        Optional<Barco> barco = barcos.stream()
-            .filter(b -> b.getId() == id)
-            .findFirst();
-        return barco.orElse(null);
-    }
-
-    // Actualizar
-    public boolean actualizarBarco(int id, double nuevaVelocidad, Posicion nuevaPosicion, Jugador nuevoJugador, Modelo nuevoModelo) {
-        Barco barco = obtenerBarcoPorId(id);
-        if (barco != null) {
-            barco.setVelocidad(nuevaVelocidad);
-            barco.setPosicion(nuevaPosicion);
-            barco.setJugador(nuevoJugador);
-            barco.setModelo(nuevoModelo);
-            return true;
-        }
-        return false;
-    }
-
-    // Eliminar
-    public boolean eliminarBarco(int id) {
-        return barcos.removeIf(b -> b.getId() == id);
-    }
-
     public List<BarcoDTO> listarBarcos() {
         // TODO Encapsular esto en el PersonMapper
         List<BarcoDTO> barcoDTOs = new ArrayList<>();
@@ -52,5 +13,25 @@ public class BarcoService {
             barcoDTOs.add(BarcoMapper.toDTO(barco));
         }
         return barcoDTOs;
+    }
+
+    public PersonDTO recuperarBarco(Long id) {
+        return BarcoMapper.toDTO(barcoRepository.findById(id).orElseThrow());
+    }
+
+    public void crear(BarcoDTO barcoDTO) {
+        Barco entity = BarcoMapper.toEntity(barcoDTO);
+        entity.setId(null);
+        BarcoRepository.save(entity);
+    }
+
+    public void actualizarBarco(BarcoDTO barcoDTO) {
+        Barco entity = BarcoMapper.toEntity(barcoDTO);
+        // TODO Chequear que el id sea != null
+        BarcoRepository.save(entity);
+    }
+
+    public void borrarBarco(Long barcoId) {
+        BarcoRepository.deleteById(barcoId);
     }
 }
