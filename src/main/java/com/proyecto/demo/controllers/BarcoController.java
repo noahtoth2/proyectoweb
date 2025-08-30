b5e3e355e85980518513ef2085b59c709e87c3c0
noahtoth2 <juanromeroo@javeriana.edu.co>
2025-08-30 16:43:49 -0500

    wenas

--- src/main/java/com/proyecto/demo/controllers/BarcoController.java
+++ src/main/java/com/proyecto/demo/controllers/BarcoController.java
@@ -1,22 +1,79 @@
 package com.proyecto.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import com.proyecto.demo.services.BarcoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.proyecto.demo.dto.BarcoDTO;
+import com.proyecto.demo.services.BarcoService;
+
 @Controller
-@RequestMapping("/barcos")
+@RequestMapping("/barco")
 public class BarcoController {
+
     @Autowired
     private BarcoService barcoService;
 
-    @GetMapping("list")
-    public List<BarcoDTO> listarBarcos() {
-        return barcoService.listarBarcos();
+    // Listar barcos
+    @GetMapping("/list")
+    public ModelAndView listarBarcos() {
+        ModelAndView modelAndView = new ModelAndView("barco-list");
+        List<BarcoDTO> barcos = barcoService.listarBarcos();
+        modelAndView.addObject("listadoBarcos", barcos);
+        return modelAndView;
     }
-        barcoService.listarBarcos();
 
-}
+    // Ver un barco
+    @GetMapping("/view/{idBarco}")
+    public ModelAndView recuperarBarco(@PathVariable Long idBarco) {
+        ModelAndView modelAndView = new ModelAndView("barco-view");
+        BarcoDTO barco = barcoService.recuperarBarco(idBarco);
+        modelAndView.addObject("barco", barco);
+        return modelAndView;
+    }
 
-    
+    // Formulario para crear
+    @GetMapping("/create-form")
+    public ModelAndView createForm() {
+        ModelAndView modelAndView = new ModelAndView("barco-create");
+        modelAndView.addObject("barco", new BarcoDTO());
+        return modelAndView;
+    }
+
+    // Crear barco y redireccionar
+    @PostMapping("/create")
+    public RedirectView create(@ModelAttribute BarcoDTO barcoDTO) {
+        barcoService.crear(barcoDTO);
+        return new RedirectView("/barco/list");
+    }
+
+    // Formulario para editar
+    @GetMapping("/edit-form/{idBarco}")
+    public ModelAndView editForm(@PathVariable Long idBarco) {
+        ModelAndView modelAndView = new ModelAndView("barco-edit");
+        BarcoDTO barco = barcoService.recuperarBarco(idBarco);
+        modelAndView.addObject("barco", barco);
+        return modelAndView;
+    }
+
+    // Actualizar barco y redireccionar
+    @PostMapping("/update")
+    public RedirectView update(@ModelAttribute BarcoDTO barcoDTO) {
+        barcoService.actualizarBarco(barcoDTO);
+        return new RedirectView("/barco/list");
+    }
+
+    // Eliminar barco y redireccionar
+    @GetMapping("/delete/{idBarco}")
+    public RedirectView delete(@PathVariable Long idBarco) {
+        barcoService.borrarBarco(idBarco);
+        return new RedirectView("/barco/list");}
+    }
---
+++ src/main/java/com/proyecto/demo/controllers/ModeloController.java
@@ -0,0 +1,80 @@
+package com.proyecto.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.proyecto.demo.dto.ModeloDTO;
+import com.proyecto.demo.services.ModeloService;
+
+@Controller
+@RequestMapping("/modelo")
+public class ModeloController {
+
+    @Autowired
+    private ModeloService modeloService;
+
+    // Listar modelos
+    @GetMapping("/list")
+    public ModelAndView listarModelos() {
+        ModelAndView modelAndView = new ModelAndView("modelo-list");
+        List<ModeloDTO> modelos = modeloService.listarModelos();
+        modelAndView.addObject("listadoModelos", modelos);
+        return modelAndView;
+    }
+
+    // Ver un modelo
+    @GetMapping("/view/{idModelo}")
+    public ModelAndView recuperarModelo(@PathVariable Long idModelo) {
+        ModelAndView modelAndView = new ModelAndView("modelo-view");
+        ModeloDTO modelo = modeloService.recuperarJugador(idModelo);
+        modelAndView.addObject("modelo", modelo);
+        return modelAndView;
+    }
+
+    // Formulario para crear
+    @GetMapping("/create-form")
+    public ModelAndView createForm() {
+        ModelAndView modelAndView = new ModelAndView("modelo-create");
+        modelAndView.addObject("modelo", new ModeloDTO());
+        return modelAndView;
+    }
+
+    // Crear modelo y redireccionar
+    @PostMapping("/create")
+    public RedirectView create(@ModelAttribute ModeloDTO modeloDTO) {
+        modeloService.crear(modeloDTO);
+        return new RedirectView("/modelo/list");
+    }
+
+    // Formulario para editar
+    @GetMapping("/edit-form/{idModelo}")
+    public ModelAndView editForm(@PathVariable Long idModelo) {
+        ModelAndView modelAndView = new ModelAndView("modelo-edit");
+        ModeloDTO modelo = modeloService.recuperarJugador(idModelo);
+        modelAndView.addObject("modelo", modelo);
+        return modelAndView;
+    }
+
+    // Actualizar modelo y redireccionar
+    @PostMapping("/update")
+    public RedirectView update(@ModelAttribute ModeloDTO modeloDTO) {
+        modeloService.actualizarModelo(modeloDTO);
+        return new RedirectView("/modelo/list");
+    }
+
+    // Eliminar modelo y redireccionar
+    @GetMapping("/delete/{idModelo}")
+    public RedirectView delete(@PathVariable Long idModelo) {
+        modeloService.borrarModelo(idModelo);
+        return new RedirectView("/modelo/list");
+    }
+}
