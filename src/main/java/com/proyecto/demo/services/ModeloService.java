ecd426914cc01ba8be7cdefcfc6be0f79ce5d8dc
jtrejos05 <162394617+jtrejos05@users.noreply.github.com>
2025-08-30 20:44:24 +0000

    update

--- src/main/java/com/proyecto/demo/services/ModeloService.java
+++ src/main/java/com/proyecto/demo/services/ModeloService.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edu.proyecto.demo.dto.ModeloDTO;
+import com.edu.proyecto.demo.mappers.ModeloMapper;
+import com.edu.proyecto.demo.models.Modelo;
+import com.edu.proyecto.demo.repository.ModeloRepository;
+
+@Service
+public class ModeloService {
+    @Autowired
+    private ModeloRepository modeloRepository;
+
+    public List<ModeloDTO> listarModelos() {
+        List<ModeloDTO> modeloDTOs = new ArrayList<>();
+        for (Modelo modelo : modeloRepository.findAll()) {
+            jugadorDTOs.add(JugadorMapper.toDTO(jugador));
+        }
+        return jugadorDTOs;
+    }
+
+    public JugadorDTO recuperarJugador(Long id) {
+        return JugadorMapper.toDTO(jugadorRepository.findById(id).orElseThrow());
+    }
+
+    public void crear(JugadorDTO jugadorDTO) {
+        Jugador entity = JugadorMapper.toEntity(jugadorDTO);
+        entity.setId(null);
+        jugadorRepository.save(entity);
+    }
+
+    public void actualizarJugador(JugadorDTO jugadorDTO) {
+        Jugador entity = JugadorMapper.toEntity(jugadorDTO);
+        // TODO: Chequear que el id sea != null
+        jugadorRepository.save(entity);
+    }
+
+    public void borrarJugador(Long jugadorId) {
+        jugadorRepository.deleteById(jugadorId);
+    }
+}
 
 
 
