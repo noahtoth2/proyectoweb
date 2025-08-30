e06700ad62973eb06879511dc193f098614f0a34
XiomaraHerrera <x.herrerasuarez@gmail.com>
2025-08-30 21:52:22 +0000

    init

--- src/main/java/com/proyecto/demo/init/DBinitializer.java
+++ src/main/java/com/proyecto/demo/init/DBinitializer.java
@@ -4,3 +4,81 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.proyecto.demo.models.Barco;
+import com.proyecto.demo.models.Celda;
+import com.proyecto.demo.models.Jugador;
+import com.proyecto.demo.models.Modelo;
+import com.proyecto.demo.models.Posicion;
+import com.proyecto.demo.models.Tablero;
+
+import com.proyecto.demo.repository.BarcoRepository;
+import com.proyecto.demo.repository.CeldaRepository;
+import com.proyecto.demo.repository.JugadorRepository;
+import com.proyecto.demo.repository.ModeloRepository;
+import com.proyecto.demo.repository.PosicionRepository;
+import com.proyecto.demo.repository.TableroRepository;
+
+@Component
+public class DbInitializer implements CommandLineRunne{
+
+    @Autowired
+    private BarcoRepository barcoRepository;
+
+    @Autowired
+    private CeldaRepository celdaRepository;
+
+    @Autowired
+    private JugadorRepository jugadorRepository;
+
+    @Autowired
+    private ModeloRepository modeloRepository;
+
+    @Autowired
+    private PosicionRepository posicionRepository;
+
+    @Autowired
+    private TableroRepository tableroRepository;
+
+    @Override
+    public void run(String... args) throws Exception{
+
+        Modelo modelo1 = modeloRepository.save(
+                       new Modelo("buque", "verde"));
+
+        Modelo modelo2 = modeloRepository.save(
+                       new Modelo("crucero", "blanco"));
+
+
+
+        Jugador jugador1 = jugadorRepository.save(
+                         new Jugador("Juan"));
+
+        Jugador jugador2 = jugadorRepository.save(
+                         new Jugador("Martin"));
+
+        Jugador jugador3 = jugadorRepository.save(
+                         new Jugador("Laura"));
+
+        Jugador jugador4 = jugadorRepository.save(
+                         new Jugador("Julian"));
+
+        Jugador jugador5 = jugadorRepository.save(
+                         new Jugador("Danna"));
+
+        Jugador jugador6 = jugadorRepository.save(
+                         new Jugador("Camilo"));
+
+
+
+        Posicion posicionini = posicionRepository.save(
+                         new Posicion("0","0"));
+
+        
+    }
+
+
+
+
+}
+
+
