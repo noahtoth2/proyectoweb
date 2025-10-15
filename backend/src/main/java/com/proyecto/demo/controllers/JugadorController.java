a4eaff0e0dd100357334932abba430e331b7cd3a
XiomaraHerrera <x.herrerasuarez@gmail.com>
2025-10-15 04:17:24 +0000

    cross

--- backend/src/main/java/com/proyecto/demo/controllers/JugadorController.java
+++ backend/src/main/java/com/proyecto/demo/controllers/JugadorController.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/jugador")
-@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Controlador de Jugadores", description = "Gestiona todas las operaciones CRUD para los jugadores del juego, incluyendo registro, consulta, actualización y eliminación de jugadores")
 public class JugadorController {
 
--- backend/src/main/java/com/proyecto/demo/controllers/PosicionController.java
+++ backend/src/main/java/com/proyecto/demo/controllers/PosicionController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +24,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/posicion")
-@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Controlador de Posiciones", description = "Gestiona todas las operaciones CRUD para las posiciones de los barcos en el tablero, incluyendo coordenadas X e Y")
 public class PosicionController {
 
